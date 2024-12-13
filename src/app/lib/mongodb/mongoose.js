87516bd243fba6a9229000c5659c5a32d71e448c
Abhishekrgadad <abhishekgadad485@gmail.com>
2024-12-13 10:33:23 -0800

    added models and connected mongodb

---
+++ src/app/lib/mongodb/mongoose.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+let initialized = false;
+
+
+export const connect = async() =>{
+    mongoose.set('strictQuery', true);
+    if(initialized){
+        console.log('Already connected to mongodb');
+        return;
+    }
+
+    try{
+        await mongoose.connect(process.env.MONGODB_URI, {
+            dbName: 'nextjsblog',
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('Connected to mongodb');
+        initialized = true;
+    } catch(error){
+        console.log('Error connecting to MongoDB', error);
+    }
+};
+
